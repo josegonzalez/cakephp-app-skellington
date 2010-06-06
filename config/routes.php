c30d3315dc1f853b97f4dd121241427342dfacd5
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-06 18:55:46 -0400

    Router::parseExtensions('json', 'xml');

--- config/routes.php
+++ config/routes.php
@@ -22,6 +22,8 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
+	Router::parseExtensions('json', 'xml');
+
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
