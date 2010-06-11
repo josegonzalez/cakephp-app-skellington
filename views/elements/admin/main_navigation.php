3e6950199e57da5c1335d325a6551a24cf463e6e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-11 16:19:18 -0400

    Advanced Main Navigation creation with caching

--- views/elements/admin/main_navigation.php
+++ views/elements/admin/main_navigation.php
@@ -1,11 +1,23 @@
 <li class="first"><?php echo $this->Html->link('Main Page', '/'); ?></li>
 <?php
-	$controllers = array_diff(App::objects('controller'),
-		array('App', 'LoginTokens', 'Lost', 'Pages', 'Statuses'));
+	if (($controllers = Cache::read('main_navigation')) === false) {
+		$controllers = array_diff(App::objects('controller'),
+			array('App', 'LoginTokens', 'Lost', 'Pages', 'Statuses'));
+		foreach ($controllers as $key => $controller_name) {
+			$model = ClassRegistry::init(Inflector::singularize($controller_name));
+			$schema = $model->schema();
+			$controllers[$key] = array(
+			    'title' => $controller_name,
+			    'url' => Inflector::tableize($controller_name)
+			);
+			if (count($schema) == 2 && (isset($schema['id']) && isset($schema['name']))) unset($controllers[$key]);
+		}
+		Cache::write('main_navigation', $controllers);
+	}
 	$i = 0;
 	foreach ($controllers as $controller) {
-		$class = ($this->params['controller'] == Inflector::tableize($controller)) ? ' class="active"' : null;
-		echo "<li{$class}>" . $this->Html->link($controller, array(
-			'plugin' => null, 'controller' => Inflector::tableize($controller), 'action' => 'index')) . '</li>';
+		$class = ($this->params['controller'] == $controller['url']) ? ' class="active"' : null;
+		echo "<li{$class}>" . $this->Html->link($controller['title'], array(
+			'plugin' => null, 'controller' => $controller['url'], 'action' => 'index')) . '</li>';
 	}
 ?>
