21e8dadac3491cca13b6db3a58f3cff92628dafd
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-15 17:56:44 -0600

    AppError::missingMethod() AppError::missingModelMethod() AppError::uninitializedClass()

--- app_error.php
+++ app_error.php
@@ -46,5 +46,62 @@ class AppError extends ErrorHandler {
 		));
 		$this->_outputMessage('assertion');
 	}
+
+/**
+ * Renders the Missing Action web page.
+ *
+ * @param array $params Parameters for controller
+ * @access public
+ * @author Jose Diaz-Gonzalez
+ */
+	function missingMethod($params) {
+		extract($params, EXTR_OVERWRITE);
+
+		$this->controller->set(array(
+			'className' => $className,
+			'methodName' => $methodName,
+			'parameters' => $parameters,
+			'parentClass' => $parentClass,
+			'path' => $path,
+			'title' => __('Missing Method in Class', true)
+		));
+		$this->_outputMessage('missingMethod');
+	}
+
+/**
+ * Renders the Missing Model Method web page
+ *
+ * @param array $params Parameters for controller
+ * @access public
+ * @author Jose Diaz-Gonzalez
+ */
+	function missingModelMethod($params) {
+		extract($params, EXTR_OVERWRITE);
+
+		$this->missingMethod(array(
+			'className' => $className,
+			'methodName' => $methodName,
+			'parameters' => $parameters,
+			'parentClass' => 'AppModel',
+			'path' => 'models'
+		));
+	}
+
+/**
+ * Renders the Uninitialized Class web page.
+ *
+ * @param array $params Parameters for controller
+ * @access public
+ * @author Jose Diaz-Gonzalez
+ */
+	function uninitializedClass($params) {
+		extract($params, EXTR_OVERWRITE);
+
+		$this->controller->set(array(
+			'className' => $className,
+			'title' => __('Failed to initialize Class', true)
+		));
+		$this->_outputMessage('uninitializedClass');
+	}
 }
 ?>
