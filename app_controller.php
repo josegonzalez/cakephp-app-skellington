f07de24cd395b0ab868eb71f5279b30c003b4b0a
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-15 22:06:08 -0600

    Docblocking

--- app_controller.php
+++ app_controller.php
@@ -8,6 +8,17 @@
  * @package       app
  */
 class AppController extends Controller {
+
+/**
+ * Array containing the names of components this controller uses. Component names
+ * should not contain the "Component" portion of the classname.
+ *
+ * Example: `var $components = array('Session', 'RequestHandler', 'Acl');`
+ *
+ * @var array
+ * @access public
+ * @link http://book.cakephp.org/view/961/components-helpers-and-uses
+ */
 	var $components = array(
 		'Authsome.Authsome' => array('model' => 'User'),
 		'DebugKit.Toolbar' => array('panels' => array(
@@ -21,9 +32,30 @@ class AppController extends Controller {
 		'Settings.Settings',
 		'Webservice.Webservice'
 	);
+
+/**
+ * An array containing the names of helpers this controller uses. The array elements should
+ * not contain the "Helper" part of the classname.
+ *
+ * Example: `var $helpers = array('Html', 'Javascript', 'Time', 'Ajax');`
+ *
+ * @var mixed A single name as a string or a list of names as an array.
+ * @access protected
+ * @link http://book.cakephp.org/view/961/components-helpers-and-uses
+ */
 	var $helpers = array(
 		'Form', 'Html', 'Js', 'Resource', 'Sanction.Clearance',
 		'Session', 'Time', 'Wysiwyg.Tinymce',  'UploadPack.Upload'
 	);
+
+/**
+ * Used to create cached instances of models a controller uses.
+ * When set to true, all models related to the controller will be cached.
+ * This can increase performance in many cases.
+ *
+ * @var boolean
+ * @access public
+ */
+	var $persistModel = true;
 }
 ?>
