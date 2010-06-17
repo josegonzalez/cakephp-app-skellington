c2d7b1c4d829d47099d70cb499e978857e241615
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-16 20:45:06 -0600

    Removing extra h2 tags as these are output in the template

--- app_helper.php
+++ app_helper.php
@@ -11,16 +11,11 @@ class AppHelper extends Helper {
 	var $view = null;
 
 	function h2($contents, $alternate = null) {
-		ob_start();
-		if ((empty($contents) || $contents == '' || $contents == ' ') && isset($alternate)) {
-			$contents = $alternate;
-		}
-		if (!$this->view) {
-			$this->view = ClassRegistry::getObject('view');
-		}
-		echo "<h2>{$contents}</h2>";
+		if ((empty($contents) || $contents == '' || $contents == ' ') && isset($alternate)) $contents = $alternate;
+		if (!$this->view) $this->view = ClassRegistry::getObject('view');
+
 		$this->view->set('title_for_layout', "{$contents} |");
-		$this->view->set("h2_for_layout", ob_get_clean());
+		$this->view->set("h2_for_layout", $contents);
 	}
 }
 ?>
