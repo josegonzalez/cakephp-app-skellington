744a43f6fbaaed003078d3346f2018b865ea8497
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-16 20:34:42 -0600

    LoggingComponent no longer conflicts with interactive portions of the DebugKit

--- plugins/log/controllers/components/logging.php
+++ plugins/log/controllers/components/logging.php
@@ -7,14 +7,13 @@ class LoggingComponent extends Object {
  * @access public
  */
 	function initialize(&$controller, $settings = array()) {
-		if (count($controller->uses) && $controller->{$controller->modelClass}->Behaviors->attached('Logable')) {
-			if (!Authsome::get('guest')) {
-				$controller->{$controller->modelClass}->setUserData(Authsome::get());
-			}
-			$controller->{$controller->modelClass}->setRequestParameters($controller->params);
-			$controller->{$controller->modelClass}->setUserBrowser($_SERVER['HTTP_USER_AGENT']);
-			$controller->{$controller->modelClass}->setUserIp($_SERVER['REMOTE_ADDR']);
-		}
+		if (!count($controller->uses) || get_parent_class($controller->{$controller->modelClass}) == 'Object') return;
+		if (!$controller->{$controller->modelClass}->Behaviors->attached('Logable')) return;
+
+		if (!Authsome::get('guest')) $controller->{$controller->modelClass}->setUserData(Authsome::get());
+		$controller->{$controller->modelClass}->setRequestParameters($controller->params);
+		$controller->{$controller->modelClass}->setUserBrowser($_SERVER['HTTP_USER_AGENT']);
+		$controller->{$controller->modelClass}->setUserIp($_SERVER['REMOTE_ADDR']);
 	}
 }
 ?>
