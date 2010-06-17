4267c1efe9b57e27be951042a4f8c02de41d7d7b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-06-17 19:33:25 -0400

    Using Default Flash element in with PermitComponent

--- config/permit.php
+++ config/permit.php
@@ -5,16 +5,16 @@ if (!class_exists('Permit')) App::import('Component', 'Sanction.PermitComponent'
 Permit::access(
 	array('plugin' => 'settings'),
 	array('auth' => array('group' => 'admin')),
-	array('redirect' => array('plugin' => null, 'controller' => 'users', 'action' => 'login')));
+	array('redirect' => array('plugin' => null, 'controller' => 'users', 'action' => 'login'), 'element' => 'flash/error'));
 
 Permit::access(
-	array('controller' => 'users', 'action' => array('change_password', 'dashboard', 'logout')),
+	array('controller' => 'users', 'action' => array('change_password', 'dashboard', 'profile', 'logout')),
 	array('auth' => true),
-	array('redirect' => array('plugin' => null, 'controller' => 'users', 'action' => 'login')));
+	array('redirect' => array('plugin' => null, 'controller' => 'users', 'action' => 'login'), 'element' => 'flash/error'));
 
 Permit::access(
 	array('controller' => 'users', 'action' => array('forgot_password', 'login', 'reset_password')),
 	array('auth' => false),
-	array('redirect' => array('plugin' => null, 'controller' => 'users', 'action' => 'dashboard')));
+	array('redirect' => array('plugin' => 'log', 'controller' => 'logs', 'action' => 'index'), 'element' => 'flash/error'));
 
 ?>
