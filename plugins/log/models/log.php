59aad7aa09e63749c9b53cc79a827dae190d48ce
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-07-28 00:54:37 -0400

    Log belongsTo User Log order by created DESC Log::find('dashboard')

--- plugins/log/models/log.php
+++ plugins/log/models/log.php
@@ -1,6 +1,14 @@
 <?php
 class Log extends LogAppModel {
 	var $name = 'Log';
-	var $order = 'created DESC';
+	var $order = 'Log.created DESC';
+	var $belongsTo = array('User');
+
+	function __findDashboard() {
+		return $this->find('all', array(
+			'contain' => array('User'),
+			'limit' => 20
+		));
+	}
 }
 ?>
